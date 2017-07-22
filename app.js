368dc028c3786b4dad4ad4488cb3f0a8151b11f4
Dan German <dangerman909@gmail.com>
2017-07-22 07:52:46 +0300

    added access token authorization for new posts

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var MongoClient = require('mongodb').MongoClient
+var ObjectId = require('mongodb').ObjectId;
+var https = require('https')
+
 var app = express()
 
 const DATABASE_URL = "mongodb://exampleuser:exampleuser@ds139082.mlab.com:39082/blog-posts"
 
 var db
-var ObjectId = require('mongodb').ObjectId;
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
@@ -30,26 +32,43 @@ app.get('/api/getposts', (req, res) => {
 })
 
 app.post('/api/newpost', (req, res) => {
-  db.collection('posts').save(req.body, (err, result) => {
-    if (err)
-      return console.log(err)
-    res.send()
+
+  const access_token = req.body.access_token
+  const host = 'graph.facebook.com'
+  const path = '/me?access_token=' + access_token
+
+  https.get({
+    hostname: host,
+    path: path,
+    agent: false
+  }, (res) => {
+    if (res.statusCode == 200) {
+      newPost()
+    }
   })
+
+  function newPost() {
+    db.collection('posts').save(req.body, (err, result) => {
+      if (err)
+        return console.log(err)
+      res.send()
+    })
+  }
 })
 
 app.put('/api/newcomment', (req, res) => {
-  db.collection('posts').findAndModify(
-    {
-      "_id":  ObjectId(req.body._id)
-    },
-    [],
-    {
-      "$push": {
-        "comments": {"text": req.body.comment, "user_id": req.body.user_id}
-      }},
-      {upsert: true}
-
-  , (err, object) => {
+  db.collection('posts').findAndModify({
+    "_id": ObjectId(req.body._id)
+  }, [], {
+    "$push": {
+      "comments": {
+        "text": req.body.comment,
+        "user_id": req.body.user_id
+      }
+    }
+  }, {
+    upsert: true
+  }, (err, object) => {
     if (err)
       return console.log(err)
   })
